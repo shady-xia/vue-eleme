b1cb97c2037f62b557503bccc751f4f54490d47b
shady-xia <499650191@qq.com>
2017-03-03 15:41:49 +0800

    3.3

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+        <!-- 使用 router-link 组件来导航. -->
+        <!-- 通过传入 `to` 属性指定链接. -->
+        <router-link to="/goods" class="tab-item">商品</router-link>
+        <router-link to="/ratings" class="tab-item">评价</router-link>
+        <router-link to="/seller" class="tab-item">商家</router-link>
+    </div>
+     <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from './components/header/header';
+
+export default {
+    components: {
+        'v-header': header
+    }
+};
+</script>
+
+<style lang="less" rel="stylesheet/less">
+@import "./common/less/mixin.less";
+
+.tab {
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+    width: 100%;
+    .border-1px(rgba(7,17,27,.1));
+}
+
+.tab-item {
+    flex: 1;
+    text-align: center;
+    color: rgb(77,85,93);
+    font-size: 14px;
+
+    &.active {
+         color: rgb(240,20,20);
+    }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import Router from 'vue-router';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+import './common/less/index.less';
+
+Vue.config.productionTip = false;
+
+Vue.use(Router);
+
+let routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller }
+];
+
+// 创建 router 实例，然后传 `routes` 配置
+let router = new Router({
+  routes,
+  'linkActiveClass': 'active'
+});
+
+// 初始导航至goods
+router.push('/goods');
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+});
